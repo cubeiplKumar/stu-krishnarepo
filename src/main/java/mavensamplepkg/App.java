58d633a4a487968b06f6e7c3a8d476ed6683aaf2
HariQedgeGithub <hariandhavarapu@gmail.com>
2022-05-28 15:44:03 +0530

    Java releaated information was added

---
+++ src/main/java/mavensamplepkg/App.java
@@ -0,0 +1,13 @@
+package mavensamplepkg;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
